8cc7a32a5855760ffff30686a880352da4bf28ae
Anish Goyal <anishgoyal@outlook.com>
2022-03-26 18:12:24 -0400

    Force kiosk script to wait until services are available

--- service_configuration/kiosk.sh
+++ service_configuration/kiosk.sh
@@ -4,6 +4,21 @@
 # Although, it does look fine in the dimmed state - could just keep it like this
 sudo bash -c "echo 0 > /sys/class/backlight/rpi_backlight/brightness"
 
+# Wait for the UI to load, so that we don't open a blank chromium window
+until curl http://localhost:8001/thermostat.html
+do
+    echo "Waiting for interface to start"
+    sleep 1
+done
+
+# Wait for the API to load as well, to ensure that the UI has data to drive
+# it
+until curl http://localhost:8001/api/config
+do
+    echo "Waiting for management service to start"
+    sleep 1
+done
+
 /usr/bin/chromium-browser \
     --app=http://localhost:8081/thermostat.html \
     --kiosk \
