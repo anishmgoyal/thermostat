7e65151ff97c3a8e8b5fbb6802b7e9ccf3735a0b
Anish Goyal <anishgoyal@outlook.com>
2022-03-25 23:54:50 -0400

    Bump version for test update

--- download_update.sh
+++ download_update.sh
@@ -21,7 +21,7 @@ cd thermostat
 # Check if update needed. If not, clean up and exit
 # the script
 NEW_HASH=$(git show --pretty='format:%H' HEAD | head -1)
-if [[ '8ef37f061cdad50c6f9318f49f5841953c46aedc' == $NEW_HASH ]]
+if [[ 'e58cb60806d03d1e57196b103a5cf04e5b7c223c' == $NEW_HASH ]]
 then
     do_cleanup
     exit 0
