1b354a9732139de5ec7fc160f79bb2cfaa72a182
Anish Goyal <anishgoyal@outlook.com>
2022-03-25 20:39:53 -0400

    Fix null sensor data updates

--- thermostat_controller/evaluator/evaluator.py
+++ thermostat_controller/evaluator/evaluator.py
@@ -16,27 +16,30 @@ class Evaluator(object):
             schedule: data.Schedule,
             sensor: circuit.TemperatureSensor):
         # common logic for updating sensor data in clients
-        try:
-            base_ev = {
-                mqttconstants.CFG_EVENT_TYPE:
-                    mqttconstants.EVENT_SENSOR_READING,
-                mqttconstants.CFG_SENSOR_ID:
-                    mqttconstants.SENSOR_ID_MAIN_SENSOR
-            }
+        base_ev = {
+            mqttconstants.CFG_EVENT_TYPE:
+                mqttconstants.EVENT_SENSOR_READING,
+            mqttconstants.CFG_SENSOR_ID:
+                mqttconstants.SENSOR_ID_MAIN_SENSOR
+        }
+        
+        temp_c = sensor.getTemperature()
+        if temp_c is not None:
             mqtt_client.sendEvent({
                 **base_ev,
                 mqttconstants.CFG_SENSOR_TYPE:
                     mqttconstants.SENSOR_TYPE_TEMPERATURE,
-                mqttconstants.CFG_SENSOR_VALUE: sensor.getTemperature()
+                mqttconstants.CFG_SENSOR_VALUE: temp_c
             })
+        
+        humid = sensor.getHumidity()
+        if humid is not None:
             mqtt_client.sendEvent({
                 **base_ev,
                 mqttconstants.CFG_SENSOR_TYPE:
                     mqttconstants.SENSOR_TYPE_HUMIDITY,
-                mqttconstants.CFG_SENSOR_VALUE: sensor.getHumidity()
+                mqttconstants.CFG_SENSOR_VALUE: humid
             })
-        except:
-            logging.exception("Failed to get temperature from sensor")
 
         # common logic for checking the state of the fan
         if run_data.getFanEnabled() and not controller.is_fan_on:
