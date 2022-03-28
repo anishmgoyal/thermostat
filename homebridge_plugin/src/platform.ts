625a52550960f997dc6721f1921f38d794c8ca46
Anish Goyal <anishgoyal@outlook.com>
2022-03-28 14:56:48 -0400

    Add homebridge plugin

---
+++ homebridge_plugin/src/platform.ts
@@ -0,0 +1,71 @@
+import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, Service, Characteristic, PlatformConfig } from 'homebridge';
+
+import { PLATFORM_NAME, PLUGIN_NAME, ThermostatPlatformConfig } from './settings';
+import { ThermostatPlatformAccessory } from './platformAccessory';
+
+/**
+ * ThermostatHomebridgePlatform
+ * Parses user configuration for thermostats
+ */
+export class ThermostatHomebridgePlatform implements DynamicPlatformPlugin {
+  public readonly Service: typeof Service = this.api.hap.Service;
+  public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
+
+  public readonly config: ThermostatPlatformConfig;
+
+  // this is used to track restored cached accessories
+  public readonly accessories: PlatformAccessory[] = [];
+
+  constructor(
+    public readonly log: Logger,
+    config: PlatformConfig,
+    public readonly api: API,
+  ) {
+    this.config = config as ThermostatPlatformConfig;
+    this.log.debug('Finished initializing platform:', this.config.name);
+
+    // When this event is fired it means Homebridge has restored all cached accessories from disk.
+    // Dynamic Platform plugins should only register new accessories after this event was fired,
+    // in order to ensure they weren't added to homebridge already. This event can also be used
+    // to start discovery of new accessories.
+    this.api.on('didFinishLaunching', () => {
+      log.debug('Executed didFinishLaunching callback');
+      // run the method to discover / register your devices as accessories
+      this.discoverDevices();
+    });
+  }
+
+  /**
+   * This function is invoked when homebridge restores cached accessories from disk at startup.
+   * It should be used to setup event handlers for characteristics and update respective values.
+   */
+  configureAccessory(accessory: PlatformAccessory) {
+    this.log.info('Loading accessory from cache:', accessory.displayName);
+
+    // add the restored accessory to the accessories cache so we can track if it has already been registered
+    this.accessories.push(accessory);
+  }
+
+  /**
+   * Register thermostats enumerated in the homebridge config file
+   */
+  discoverDevices() {
+    for (const device of this.config.thermostats) {
+      const uuid = this.api.hap.uuid.generate(device.hostname);
+      const existingAccessory = this.accessories.find(
+        accessory => accessory.UUID === uuid);
+      if (existingAccessory) {
+        this.log.info('Restoring existing accessory from cache:',
+          existingAccessory.displayName);
+        new ThermostatPlatformAccessory(this, existingAccessory);
+      } else {
+        this.log.info('Adding new accessory:', device.displayName);
+        const accessory = new this.api.platformAccessory(
+          device.displayName, uuid);
+        accessory.context.device = device;
+        new ThermostatPlatformAccessory(this, accessory);
+        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+      }
+    }
+  }
+}
---
+++ homebridge_plugin/src/platformAccessory.ts
@@ -0,0 +1,198 @@
+import { Service, PlatformAccessory, CharacteristicValue, Perms } from 'homebridge';
+import { firstValueFrom } from 'rxjs';
+
+import { ThermostatHomebridgePlatform } from './platform';
+import { ThermostatApi, ThermostatState } from './thermostatApi';
+
+/**
+ * Platform Accessory
+ * An instance of this class is created for each accessory your platform registers
+ * Each accessory may expose multiple services of different service types.
+ */
+export class ThermostatPlatformAccessory {
+  private hostname: string;
+  private fanService: Service;
+  private thermostatService: Service;
+  private api: ThermostatApi;
+
+  // Aliases for supported characteristics
+  readonly Active =
+    this.platform.Characteristic.Active;
+  readonly TargetFanState =
+    this.platform.Characteristic.TargetFanState;
+  readonly CurrHeatCool =
+    this.platform.Characteristic.CurrentHeatingCoolingState;
+  readonly TargetHeatCool =
+    this.platform.Characteristic.TargetHeatingCoolingState;
+  readonly CurrTemp =
+    this.platform.Characteristic.CurrentTemperature;
+  readonly TargetTemp =
+    this.platform.Characteristic.TargetTemperature;
+  readonly TempDisplayUnits =
+    this.platform.Characteristic.TemperatureDisplayUnits;
+  readonly CoolThresh =
+    this.platform.Characteristic.CoolingThresholdTemperature;
+  readonly HeatThresh =
+    this.platform.Characteristic.HeatingThresholdTemperature;
+
+  constructor(
+    private readonly platform: ThermostatHomebridgePlatform,
+    private readonly accessory: PlatformAccessory,
+  ) {
+    this.hostname = accessory.context.device.hostname;
+    this.api = new ThermostatApi(this.platform, this.hostname);
+
+    // Set minimal accessory properties
+    this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Bland Devices')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Thermostat')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, this.hostname);
+
+    this.fanService =
+      this.accessory.getService(this.platform.Service.Fanv2) ||
+      this.accessory.addService(this.platform.Service.Fanv2);
+
+    this.fanService.setCharacteristic(this.platform.Characteristic.Name,
+      `${accessory.displayName} Fan`);
+
+    this.fanService.getCharacteristic(this.Active)
+      .setProps({perms: [Perms.EVENTS, Perms.PAIRED_READ]});
+
+    this.fanService.getCharacteristic(this.TargetFanState)
+      .onSet(this.setFanState.bind(this));
+
+    this.thermostatService =
+      this.accessory.getService(this.platform.Service.Thermostat) ||
+      this.accessory.addService(this.platform.Service.Thermostat);
+
+    this.thermostatService.setCharacteristic(this.platform.Characteristic.Name,
+      accessory.displayName);
+
+    this.thermostatService.getCharacteristic(this.TargetHeatCool)
+      .onSet(this.setTargetHeatingCoolingState.bind(this));
+
+    this.thermostatService.getCharacteristic(this.TargetTemp)
+      .onSet(this.setTargetTemperature.bind(this))
+      .setProps({minValue: 17, maxValue: 28})
+      .updateValue(17);
+
+    this.thermostatService.getCharacteristic(this.TempDisplayUnits)
+      .onGet(() => this.TempDisplayUnits.FAHRENHEIT);
+
+    this.thermostatService.getCharacteristic(this.CoolThresh)
+      .onSet(this.setCoolingThresholdTemperature.bind(this))
+      .setProps({minValue: 17, maxValue: 28})
+      .updateValue(17);
+
+    this.thermostatService.getCharacteristic(this.HeatThresh)
+      .onSet(this.setHeatingThresholdTemperature.bind(this))
+      .setProps({minValue: 17, maxValue: 28})
+      .updateValue(17);
+
+    this.api.state$.subscribe(state => this.updateCharacteristics(state));
+  }
+
+  async setFanState(value: CharacteristicValue) {
+    let fanState: ThermostatState['fanState'] = 'auto';
+    switch (value) {
+      case this.TargetFanState.MANUAL:
+        fanState = 'on';
+        break;
+      case this.TargetFanState.AUTO:
+        fanState = 'auto';
+        break;
+    }
+    this.platform.log.debug(`Set fan state: ${fanState}`);
+    await firstValueFrom(this.api.setFanState(fanState));
+  }
+
+  async setTargetHeatingCoolingState(value: CharacteristicValue) {
+    let targetState: ThermostatState['targetHeatingCoolingState'] = 'off';
+    switch (value) {
+      case this.TargetHeatCool.OFF:
+        targetState = 'off';
+        break;
+      case this.TargetHeatCool.COOL:
+        targetState = 'cool';
+        break;
+      case this.TargetHeatCool.HEAT:
+        targetState = 'heat';
+        break;
+      case this.TargetHeatCool.AUTO:
+        targetState = 'auto';
+        break;
+    }
+    this.platform.log.debug(`Set target heat/cool state: ${targetState}`);
+    await firstValueFrom(this.api.setTargetHeatingCoolingState(targetState));
+  }
+
+  async setTargetTemperature(value: CharacteristicValue) {
+    this.platform.log.debug(`Set target temperature: ${value as number}`);
+    await firstValueFrom(this.api.setTargetTemperature(value as number));
+  }
+
+  async setCoolingThresholdTemperature(value: CharacteristicValue) {
+    this.platform.log.debug(`Set cooling threshold: ${value as number}`);
+    await firstValueFrom(this.api.setCoolingThresholdTemp(value as number));
+  }
+
+  async setHeatingThresholdTemperature(value: CharacteristicValue) {
+    this.platform.log.debug(`Set heating threshold: ${value as number}`);
+    await firstValueFrom(this.api.setHeatingThresholdTemp(value as number));
+  }
+
+  private updateCharacteristics(thermostatState: ThermostatState) {
+    this.platform.log.debug(
+      `Updating characteristics: ${JSON.stringify(thermostatState)}`);
+    const currFanActive =
+      thermostatState.fanState === 'on' ||
+      thermostatState.currentHeatingCoolingState !== 'off' ?
+        this.Active.ACTIVE :
+        this.Active.INACTIVE;
+    this.fanService.updateCharacteristic(
+      this.Active, currFanActive);
+
+    const targetFanState =
+      thermostatState.fanMode === 'on' ?
+        this.TargetFanState.MANUAL :
+        this.TargetFanState.AUTO;
+    this.fanService.updateCharacteristic(
+      this.TargetFanState, targetFanState);
+
+    const currHeatCool = {
+      'off': this.CurrHeatCool.OFF,
+      'heat': this.CurrHeatCool.HEAT,
+      'cool': this.CurrHeatCool.COOL,
+    }[thermostatState.currentHeatingCoolingState];
+    this.thermostatService.updateCharacteristic(
+      this.CurrHeatCool, currHeatCool);
+
+    const targetHeatCool = {
+      'off': this.TargetHeatCool.OFF,
+      'heat': this.TargetHeatCool.HEAT,
+      'cool': this.TargetHeatCool.COOL,
+      'auto': this.TargetHeatCool.AUTO,
+    }[thermostatState.targetHeatingCoolingState];
+    this.thermostatService.updateCharacteristic(
+      this.TargetHeatCool, targetHeatCool);
+
+    this.thermostatService.updateCharacteristic(
+      this.CurrTemp, thermostatState.currentTemperature);
+
+    if (thermostatState.targetTemperature != null) {
+      this.thermostatService.updateCharacteristic(
+        this.TargetTemp, thermostatState.targetTemperature);
+    }
+
+    if (thermostatState.coolingThresholdTemperature != null) {
+      this.thermostatService.updateCharacteristic(
+        this.CoolThresh, thermostatState.coolingThresholdTemperature);
+    }
+
+    if (thermostatState.heatingThresholdTemperature != null) {
+      this.thermostatService.updateCharacteristic(
+        this.HeatThresh, thermostatState.heatingThresholdTemperature);
+    }
+  }
+
+}
